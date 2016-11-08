3c5566198656c70d5c99211ea3eb826aa34f61cd
xm94630 <xm94630@126.com>
2016-11-08 20:33:25 +0800

    8:init方法中，对传入的参数进行分门别类的筛选

--- app/note2.js
+++ app/note2.js
@@ -18,3 +18,68 @@ jQuery.fn = jQuery.prototype = {
 	   $([]) $({})
 
 }
+
+//init中传入参数之后，根据类型进行if分类处理。
+//下面是可能有的所有类型
+$("") $(null) $(undefined) $(false)
+$('#div') $('.box') $('div') $('#div div.box')
+$('<li>') $('<li>1</li><li>2</li>') 
+$('<li>Hello') 
+$(this) $(document)
+$(function(){})
+$([]) $({})
+
+//对上面的进行条件处理之后呢，得到的结果活存放在 match 的变量之中
+if(){
+
+	match = [null,'<li>',null]  //$('<li>')
+	match = [null,'<li>1</li><li>2</li>',null]  //$('<li>1</li><li>2</li>')
+
+}else{
+	match = null ; //$('.box') $('div') $('#div div.box')
+	
+	//这个其实是进行了 /正则/.exec('参数') 的操作
+	//之前我已经研究过 exec 的用法，所以非常好理解结果
+	match = ['#div',null,'div1'] ; //$('#div')
+	match = ['<li>Hello','<li>','null'] ; //$('<li>Hello')
+}
+
+if(){ //能进入的是创建标签的和id的 $('#div') $('<li>')
+
+	//进一步判断 
+	if(){ $('<li>') }else{ $('#div') }
+
+}else{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
