405c45795611cf6891c67d801cd06bd2ab8ad25c
xuming3 <xuming3@yiche.com>
2016-11-08 18:06:16 +0800

    添加笔记：jquery 实例方法中的参数处理，判断参数类型。

--- app/note2.js
+++ app/note2.js
@@ -2,5 +2,19 @@
 jQuery.fn = jQuery.prototype = {
 	jquery 版本
 	constructor 指向jQery
-	
+	init() 初始化、参数
+	   对参数的处理
+	   $("") $(null) $(undefined) $(false)
+	   对字符串的参数处理
+	       选择
+		   $('#div') $('.box') $('div') $('#div div.box')
+		   还可以创建元素
+		   $('<li>')
+	   对dom元素处理了
+	   $(this) $(document)
+	   传函数
+	   $(function(){})
+	   传数组 传对象
+	   $([]) $({})
+
 }
